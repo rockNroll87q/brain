990c73d5860805417567ddaf088f89ddb7172c85
dibz15 <austin.dibble15@gmail.com>
2024-06-24 12:31:09 +0100

    reformat for PEP

--- metrics.py
+++ metrics.py
@@ -32,17 +32,19 @@ SOFTWARE.
 
 """
 
+import math
 
 import numpy as np
 import pandas as pd
-import math
 from sklearn.metrics import r2_score, mean_squared_error
-
 import numpy as np
 
 # Copied from src.auxiliary from original source repository
-def categorize(num_vector, bin=(18, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100),
-               int_bin=False):
+def categorize(
+    num_vector,
+    bin=(18, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100),
+    int_bin=False,
+):
     """
     Categorize each element of a numerical vector into predefined bins.
 
@@ -80,11 +82,20 @@ def categorize(num_vector, bin=(18, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 
         # skip the right most bin[-1], to include values x=bin[-1] to the last interval [bin[-2], bin[-1]]
         categorized = np.digitize(num_vector, bin[1:-1], right=False)
     else:
-        categorized = np.array([category_labels[i] for i in np.digitize(num_vector, bin[1:-1])])
+        categorized = np.array(
+            [category_labels[i] for i in np.digitize(num_vector, bin[1:-1])]
+        )
     return np.array(categorized)
 
+
 # == Begin modified code than that from original MIT-licensed author Lara in order to make it use numpy, not pandas ==
-def maxMAE(y:np.ndarray, y_pred:np.ndarray, categories=np.append(np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100]))):
+def maxMAE(
+    y: np.ndarray,
+    y_pred: np.ndarray,
+    categories=np.append(
+        np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100])
+    ),
+):
     r"""
     Compute max MAE over given categories;
     """
@@ -100,7 +111,15 @@ def maxMAE(y:np.ndarray, y_pred:np.ndarray, categories=np.append(np.append(np.ar
         mae_max.append(mae_i)
     return max(mae_max)
 
-def maxMAdE(y_cat:np.ndarray, y:np.ndarray, y_pred:np.ndarray, categories=np.append(np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100]))):
+
+def maxMAdE(
+    y_cat: np.ndarray,
+    y: np.ndarray,
+    y_pred: np.ndarray,
+    categories=np.append(
+        np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100])
+    ),
+):
     r"""
     Compute max MAdE (delta error) over given categories; Subjects are categorized based on y_cat. Max error values
      is computed based on y and y_pred values. y_cat is usually starting age ('y_1' in the original code)
@@ -117,35 +136,44 @@ def maxMAdE(y_cat:np.ndarray, y:np.ndarray, y_pred:np.ndarray, categories=np.app
         mae_max.append(mae_i)
     return max(mae_max)
 
-def pearsons_r(y:np.ndarray, y_pred:np.ndarray):
+
+def pearsons_r(y: np.ndarray, y_pred: np.ndarray):
     r"""
     Compute Pearson's correlation between two columns;
     """
     y_np = y
     y_pred_np = y_pred
     corr_mat = np.corrcoef(y_np, y_pred_np)
-    return  corr_mat[0,1] #y_true, y_pred
+    return corr_mat[0, 1]  # y_true, y_pred
 
-def RMSE(y:np.ndarray, y_pred:np.ndarray):
+
+def RMSE(y: np.ndarray, y_pred: np.ndarray):
     r"""
     RMSE between true and predicted value;
     """
     return math.sqrt(mean_squared_error(y, y_pred))
 
-def R_squared(y:np.ndarray, y_pred:np.ndarray):
+
+def R_squared(y: np.ndarray, y_pred: np.ndarray):
     r"""
     R² between true and predicted value;
     """
     return r2_score(y, y_pred)
 
+
 # == Code below here was completely added by Austin Dibble ==
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
+
 class BasicMetricsWrapper:
-    def __init__(self, y_true=None, y_pred=None, df=None, y_true_col=None, y_pred_col=None):
+    def __init__(
+        self, y_true=None, y_pred=None, df=None, y_true_col=None, y_pred_col=None
+    ):
         if all(isinstance(arg, str) for arg in [y_true_col, y_pred_col]):
             if df is None:
-                raise ValueError("If y_true_col and y_pred_col string column names, df must be provided.")
+                raise ValueError(
+                    "If y_true_col and y_pred_col string column names, df must be provided."
+                )
 
             self.y_true = df[y_true_col].values
             self.y_pred = df[y_pred_col].values
@@ -153,54 +181,58 @@ class BasicMetricsWrapper:
         elif all(isinstance(arg, np.ndarray) for arg in [y_true, y_pred]):
             if len(y_true) != len(y_pred):
                 raise ValueError("y_true and y_pred must have the same length.")
-            
+
             self.y_true = y_true
             self.y_pred = y_pred
         else:
-            raise ValueError("Either (y_true and y_pred as numpy arrays) or (df and column names as strings) must be provided.")
-        
-        self.default_bins = np.append(np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100]))
+            raise ValueError(
+                "Either (y_true and y_pred as numpy arrays) or (df and column names as strings) must be provided."
+            )
+
+        self.default_bins = np.append(
+            np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100])
+        )
 
     def mae(self):
         return mean_absolute_error(self.y_true, self.y_pred)
-    
+
     def me(self):
         return np.mean(self.y_pred - self.y_true)
 
     def mse(self):
         return mean_squared_error(self.y_true, self.y_pred)
-    
+
     def rmse(self):
         return RMSE(self.y_true, self.y_pred)
-    
+
     def r_squared(self):
         return R_squared(self.y_true, self.y_pred)
-    
+
     def pearsons_r(self):
         return pearsons_r(self.y_true, self.y_pred)
-    
+
     def maxMAE(self):
         return maxMAE(self.y_true, self.y_pred, categories=self.default_bins)
-    
-    # def maxMAdE(self):
-    #     return maxMAdE(y_cat=None, y=self.y_true, y_pred=self.y_pred, categories=self.default_bins) # TODO allow for y_cat
 
     def calculate_metrics(self):
         metrics = {
-            'MAE': self.mae(),
-            'MSE': self.mse(),
-            'RMSE': self.rmse(),
-            'r_squared': self.r_squared(),
-            'pearsons_r': self.pearsons_r(),
-            'maxMAE': self.maxMAE()
+            "MAE": self.mae(),
+            "MSE": self.mse(),
+            "RMSE": self.rmse(),
+            "r_squared": self.r_squared(),
+            "pearsons_r": self.pearsons_r(),
+            "maxMAE": self.maxMAE(),
         }
         return pd.DataFrame([metrics])
+
 
 class LongitudinalMetricsWrapper:
     def __init__(self, y1, y2, y_pred1, y_pred2, df=None):
         if all(isinstance(arg, str) for arg in [y1, y2, y_pred1, y_pred2]):
             if df is None:
-                raise ValueError("If y1, y2, y_pred1, and y_pred2 are column names, df must be provided.")
+                raise ValueError(
+                    "If y1, y2, y_pred1, and y_pred2 are column names, df must be provided."
+                )
             self.y1 = df[y1].values
             self.y2 = df[y2].values
             self.y_pred1 = df[y_pred1].values
@@ -215,10 +247,14 @@ class LongitudinalMetricsWrapper:
             self.y_pred2 = y_pred2
 
         else:
-            raise ValueError("y1, y2, y_pred1, and y_pred2 must be either all strings or all numpy arrays.")
+            raise ValueError(
+                "y1, y2, y_pred1, and y_pred2 must be either all strings or all numpy arrays."
+            )
 
-        self.default_bins = np.append(np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100]))
-        
+        self.default_bins = np.append(
+            np.append(np.array([18]), np.arange(25, 86, 10)), np.array([100])
+        )
+
         self.y_delta = self.y2 - self.y1
         self.y_pred_delta = self.y_pred2 - self.y_pred1
         self.y_pred_delta_diff = self.y_pred_delta - self.y_delta
@@ -226,35 +262,38 @@ class LongitudinalMetricsWrapper:
 
     def mde(self):
         return np.mean(self.y_pred_delta_diff)
-    
+
     def mde_sd(self):
         return np.std(self.y_pred_delta_diff)
-    
+
     def made(self):
         return np.mean(self.y_pred_delta_diff_abs)
-    
+
     def made_sd(self):
         return np.std(self.y_pred_delta_diff_abs)
 
     def maxMAdE_10y(self):
-        return self.maxMAdE(self.y1, self.y_delta, self.y_pred_delta, categories=self.default_bins)
+        return self.maxMAdE(
+            self.y1, self.y_delta, self.y_pred_delta, categories=self.default_bins
+        )
 
     def maxMAdE(self, y_cat, y, y_pred, categories):
         return maxMAdE(y_cat, y, y_pred, categories=categories)
-    
+
     def k(self):
         return self.y_pred_delta / self.y_delta
 
     def calculate_metrics(self):
         metrics = {
-            'MdE': self.mde(),
-            'MdEsd': self.mde_sd(),
-            'MAdE': self.made(),
-            'MAdEsd': self.made_sd(),
-            'maxMAdE_10y': self.maxMAdE_10y(),
-            'k': self.k()
+            "MdE": self.mde(),
+            "MdEsd": self.mde_sd(),
+            "MAdE": self.made(),
+            "MAdEsd": self.made_sd(),
+            "maxMAdE_10y": self.maxMAdE_10y(),
+            "k": self.k(),
         }
         return pd.DataFrame([metrics])
+
 
 # Example usage with a DataFrame:
 # Assuming df is your DataFrame and it contains the columns: 'y1', 'y2', 'y_pred1', 'y_pred2'
