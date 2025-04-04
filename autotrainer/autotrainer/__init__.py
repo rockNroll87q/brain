255fadf515f136fb63848e1b5d68fcc69aed0d66
dibz15 <austin.dibble15@gmail.com>
2025-04-04 16:14:34 +0100

    import fix

--- autotrainer/autotrainer/__init__.py
+++ autotrainer/autotrainer/__init__.py
@@ -2,5 +2,3 @@
 from .config_loader import ConfigLoader, ConfigValidationError, InheritanceError, _resolve_task_inheritance
 from .job_creator import JobCreator
 from .job_runner import JobRunner
-from .result_manager import ResultManager
-from .aggregation import aggregate_results
