6a3f15e9bfe406cc213f6c5e8f7a40d9606fa24c
Dibz15 <austin.dibble15@gmail.com>
2025-07-11 15:03:55 +0100

    Remove early-stopper dependency on tf

--- tests/test_early_stopper.py
+++ tests/test_early_stopper.py
@@ -5,10 +5,13 @@ Copyright (c) 2024 Austin Dibble
 This code file is for unit testing the early stopper functions from utilities/early_stopper.py
 """
 
+import sys
 import unittest
 import datetime
+import time
 
 import numpy as np
+from loguru import logger
 
 from utilities import EarlyStoppingWithTimer
 
@@ -17,6 +20,9 @@ class TestEarlyStopperFunctions(unittest.TestCase):
     """Test functions for the EarlyStoppingWithTimer utility."""
     def setUp(self):
         """Setup variables/randomness/etc."""
+        logger.remove()
+        logger.add(sys.stderr, level="ERROR")
+
         # Sample 100 values from a normal distribution for testing
         np.random.seed(184)  # For reproducibility
 
@@ -54,6 +60,15 @@ class TestEarlyStopperFunctions(unittest.TestCase):
                     seconds = day*86400 + hour*3600 + minute*60
                     earlystopper = EarlyStoppingWithTimer(monitor='val_loss', patience=15, verbose=1, timelimit=seconds)
                     self.assertAlmostEqual(seconds, earlystopper.timelimit)
+
+    def test_delay(self):
+        """Test a short time delay has the right behaviour."""
+        earlystopper = EarlyStoppingWithTimer(monitor='val_loss', patience=15, verbose=1, restore_best_weights=True, timelimit=1)
+        earlystopper.on_epoch_begin(epoch=0)
+        time.sleep(2)
+        earlystopper.on_epoch_end(epoch=1)
+        # Calculate the equivalent seconds as an integer, by hand
+        self.assertAlmostEqual(earlystopper.total_time, 2, delta=0.5)
 
 if __name__ == "__main__":
     unittest.main()
